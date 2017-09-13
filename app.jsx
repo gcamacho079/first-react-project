87587104a1fc44eee4d452c9328449d52164292a
Guadalupe Camacho <gcamacho079@gmail.com>
2017-09-12 17:02:19 -0700

    Put React on hold to code HTML/CSS

--- app.jsx
+++ app.jsx
@@ -1,7 +1,27 @@
+function Header(props) {
+  return (
+    <div className="header">
+      <h1>{props.title}</h1>
+    </div>
+  );
+}
 
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired,
+}
 
-function Application() {
-  return (<h1>Hello there</h1>);
+function Application(props) {
+  return (
+    <Header title={props.title}/>
+  );
+}
+
+Application.propTypes = {
+  title: React.PropTypes.string,
+}
+
+Application.defaultProps = {
+  title: "Camper Leaderboard"
 }
 
 ReactDOM.render(<Application />, document.getElementById('container'));
